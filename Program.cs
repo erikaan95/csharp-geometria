b572990452c2a4c642ed714d389c0d5601a73759
erikaan <ancoraerika@gmail.com>
2022-04-07 23:58:58 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using FiguraGeometrica;
+
+Console.WriteLine("csharp-geometria");
+
+Console.WriteLine("Per favore, inserisci i dati i valori (base e altezza) per generare un Rettangolo: ");
+string baseRettangolo = Console.ReadLine();
+
+Console.WriteLine("Inserisci l'altezza del rettangolo: ");
+string altezzaRettangolo = Console.ReadLine();
+
+Console.WriteLine("Inserisci il perimetro del rettangolo: ");
+int perimetroRettangolo = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci l'area del rettangolo: ");
+string areaRettangolo = Console.ReadLine();
+
+// Creo un oggetto Rettangolo //
+Rettangolo IlMioRettangolo = new Rettangolo(baseRettangolo, altezzaRettangolo,);
+
+IlMioRettangolo.StampaRettangolo();
+
+IlMioRettangolo.AssegnaUnaBase("20 cm");
+
+IlMioRettangolo.AssegnaUnaAltezza("10 cm");
+
+IlMioRettangolo.AssegnaUnPerimetro("60 cm");
+
+IlMioRettangolo.AssegnaUnArea("200 cm2");
---
+++ Rettangolo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rettangolo
+{
+    internal class Rettangolo
+    {
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+        private int areaRettangolo;
+        private int perimetroRettangolo;
+
+        // COSTRUTTORE //
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+        // Perimetro rettangolo //
+        public Rettangolo()
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+        // Stampo il metodo //
+        public void StampaRettangolo()
+        {
+            Console.WriteLine("|____________|");
+            Console.WriteLine("Base: " + baseRettangolo);
+            Console.WriteLine("Altezza: " + altezzaRettangolo);
+            Console.WriteLine("Area: " + areaRettangolo);
+            Console.WriteLine("Perimetro: " + perimetroRettangolo);
+        }
+    }
+}
